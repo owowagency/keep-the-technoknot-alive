a1231b0470f5f32fffdabd80b4521f616aa41c03
zjuulowow <zjuul@owow.io>
2025-06-04 18:19:06 +0200

    fiklajsdjlaskdfjaskldfjasldf

--- src/world.js
+++ src/world.js
@@ -40,7 +40,22 @@ ticker.start(({ elapsedTime }) => {
 	if (starvation > STARVATION_TIME)
 		eventEmitter.emit('dead')
 
-	renderImage(ctx, width, height);
+	renderImage('frames/flip.png', ctx, width, height);
+
+	{
+		const imageData = ctx.getImageData(0, 0, width, height);
+		const data = imageData.data;
+		for (let i = 0; i < data.length; i += 4) {
+			// Apply thresholding - any pixel above 127 brightness becomes white (255), otherwise black (0)
+			const brightness = (data[i] + data[i + 1] + data[i + 2]) / 3;
+			const binary = brightness > 127 ? 255 : 0;
+			data[i] = binary; // R
+			data[i + 1] = binary; // G
+			data[i + 2] = binary; // B
+			data[i + 3] = 255; // The board is not transparent :-)
+		}
+		ctx.putImageData(imageData, 0, 0);
+	}
 
 	if (IS_DEV) {
 		// Save the canvas as a PNG file
